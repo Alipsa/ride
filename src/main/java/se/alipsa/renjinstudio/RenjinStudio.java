5d315ff898d17e209d0931d4c32c179c8d99ff54
Per Nyfelt <per.nyfelt@alipsa.se>
2018-12-12 21:38:23 +0100

    better basic layout

--- src/main/java/se/alipsa/renjinstudio/RenjinStudio.java
+++ src/main/java/se/alipsa/renjinstudio/RenjinStudio.java
@@ -1,14 +1,14 @@
 package se.alipsa.renjinstudio;
 
 import javafx.application.Application;
+import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.SplitPane;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.control.TextArea;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class RenjinStudio extends Application {
@@ -20,12 +20,13 @@ public class RenjinStudio extends Application {
     @Override
     public void start(Stage primaryStage) {
 
-        GridPane root = new GridPane();
+        HBox root = new HBox();
         root.setAlignment(Pos.CENTER);
 
         Scene scene = new Scene(root, 1024, 768);
 
-        SplitPane spTop = new SplitPane();
+        SplitPane leftSplitPane = new SplitPane();
+        leftSplitPane.setOrientation(Orientation.VERTICAL);
 
         TabPane codetp = new TabPane();
         Tab codeTab = new Tab();
@@ -34,18 +35,21 @@ public class RenjinStudio extends Application {
         codeTab.setContent(code);
         codetp.getTabs().add(codeTab);
 
+        TextArea console = new TextArea();
+        console.setText("Console");
+
+        leftSplitPane.getItems().addAll(codetp, console);
+
+
+        SplitPane rightSplitPane = new SplitPane();
+        rightSplitPane.setOrientation(Orientation.VERTICAL);
+
         TabPane northEasttp = new TabPane();
         Tab northEastTab = new Tab();
         TextArea northEast = new TextArea();
         northEast.setText("North East");
         northEastTab.setContent(northEast);
         northEasttp.getTabs().add(northEastTab);
-        root.add(codetp, 1,0);
-        spTop.getItems().addAll(codetp, northEasttp);
-
-        SplitPane spBottom = new SplitPane();
-        TextArea console = new TextArea();
-        console.setText("Console");
 
         TabPane southEasttp = new TabPane();
         Tab southEastTab = new Tab();
@@ -54,10 +58,13 @@ public class RenjinStudio extends Application {
         southEastTab.setContent(southEast);
         southEasttp.getTabs().add(southEastTab);
 
-        spBottom.getItems().addAll(console, southEasttp);
+        rightSplitPane.getItems().addAll(northEasttp, southEasttp);
 
-        root.add(spTop, 0,0);
-        root.add(spBottom, 0,1);
+        SplitPane splitPane = new SplitPane();
+        splitPane.setOrientation(Orientation.HORIZONTAL);
+        splitPane.getItems().addAll(leftSplitPane, rightSplitPane);
+
+        root.getChildren().add(splitPane);
 
 
         primaryStage.setTitle("Renjin Studio");
