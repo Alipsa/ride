f0f7be76ec95e2c018d7d9d606b3b52a504e1c93
pernyf <per.nyfelt@nordnet.se>
2018-12-19 16:45:38 +0100

    stub for plot tab

---
+++ src/main/java/se/alipsa/renjinstudio/inout/PlotArea.java
@@ -0,0 +1,7 @@
+package se.alipsa.renjinstudio.inout;
+
+public class PlotArea {
+
+  // TODO consider using https://github.com/brucejohnson/jfxplot
+  // in a similar way as https://github.com/brucejohnson/studiofx
+}
