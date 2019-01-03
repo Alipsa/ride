c1305ebfdd55c38cd6fdccb9ad29023388f0a175
pernyf <per.nyfelt@nordnet.se>
2019-01-03 16:42:26 +0100

    Testing hamcrest for a a function that fails

--- src/test/R/downloadFile.R
+++ src/test/R/downloadFile.R
@@ -4,8 +4,12 @@ library("org.renjin.cran:downloader")
 url <- "http://raw.githubusercontent.com/genomicsclass/dagdata/master/inst/extdata/femaleControlsPopulation.csv"
 filename <- basename(url)
 print(filename)
+download(url, destfile=filename)
+assertThat(file.exists(filename), identicalTo(TRUE))
+
 #download(url, destfile=filename, quiet = FALSE, mode = "w", cacheOK = TRUE)
 #download.file(url, destfile = filename, method = "wget")
+# However this works:
 download.file(url, destfile = filename)
 if(file.exists(filename)) {
 	print("success!")
