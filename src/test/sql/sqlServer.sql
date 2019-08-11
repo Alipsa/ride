ed62673e83dc190a3ec6bdc17c293306519e6f3f
Per Nyfelt <per.nyfelt@alipsa.se>
2019-08-11 22:07:59 +0200

    add test sql for sql server

---
+++ src/test/sql/sqlServer.sql
@@ -0,0 +1,24 @@
+--com.microsoft.sqlserver.jdbc.SQLServerDriver
+
+DROP TABLE IF EXISTS #creditInformation;
+create table #creditInformation (
+  id int primary key,
+  dateExtracted date,
+  message varchar(20)
+);
+
+insert into #creditInformation values (1, '2019-01-20', 'first row');
+insert into #creditInformation values (2, '2019-02-03', 'second row');
+declare @extractDate date = (select max(dateExtracted) from #creditInformation);
+declare @ciTable table (ciId numeric(19,0));
+
+insert into @ciTable 
+select id as ciId 
+from creditInformation 
+where dateExtracted = @extractDate;
+
+select * into testTable from @ciTable;
+
+select * from testTable
+
+--create database mydb;
