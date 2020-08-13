0267e39af6ac63798845c6b89666cc4fcf975521
pernyf <per.nyfelt@nordnet.se>
2020-08-13 16:42:34 +0200

    set serialVersionUID

--- src/main/java/se/alipsa/ride/utils/UniqueList.java
+++ src/main/java/se/alipsa/ride/utils/UniqueList.java
@@ -7,30 +7,33 @@ import java.util.Collection;
  * A naive implementation of a List containing only unique items
  * Works fine for small lists but for bigger lists something more advanced is needed due to
  * performance issues with contains methods used to guarantee uniqueness.
+ *
  * @param <T>
  */
 public class UniqueList<T> extends ArrayList<T> {
 
-    @Override
-    public boolean add(T t) {
-        if (!contains(t)) return super.add(t);
-        return false;
-    }
+  static final long serialVersionUID = 1L;
 
-    @Override
-    public void add(int index, T element) {
-        if (!contains(element)) super.add(index, element);
-    }
+  @Override
+  public boolean add(T t) {
+    if (!contains(t)) return super.add(t);
+    return false;
+  }
 
-    @Override
-    public boolean addAll(Collection<? extends T> c) {
-        if (!containsAll(c)) return super.addAll(c);
-        return false;
-    }
+  @Override
+  public void add(int index, T element) {
+    if (!contains(element)) super.add(index, element);
+  }
 
-    @Override
-    public boolean addAll(int index, Collection<? extends T> c) {
-        if (!containsAll(c)) return super.addAll(index, c);
-        return false;
-    }
+  @Override
+  public boolean addAll(Collection<? extends T> c) {
+    if (!containsAll(c)) return super.addAll(c);
+    return false;
+  }
+
+  @Override
+  public boolean addAll(int index, Collection<? extends T> c) {
+    if (!containsAll(c)) return super.addAll(index, c);
+    return false;
+  }
 }
--- src/main/java/se/alipsa/ride/utils/maven/DependenciesResolveException.java
+++ src/main/java/se/alipsa/ride/utils/maven/DependenciesResolveException.java
@@ -2,6 +2,8 @@ package se.alipsa.ride.utils.maven;
 
 public class DependenciesResolveException extends Exception {
 
+  static final long serialVersionUID = 1L;
+
   public DependenciesResolveException(String message, Throwable cause) {
     super(message, cause);
   }
