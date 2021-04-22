7882b993732cabc800f558155e508e9e61ac39a6
pernyf <per.nyfelt@alipsa.se>
2021-04-22 13:55:39 +0200

    enable running Ride from git-bash. Bump jackson and groovy versions

--- src/bin/ride.sh
+++ src/bin/ride.sh
@@ -8,7 +8,14 @@ function notify() {
     zenity --info --text="$1"
   elif command -v notify-send > /dev/null 2>&1; then
     notify-send "$1"
+  elif [[ "${OSTYPE}" == "msys" ]]; then
+    msg ${USERNAME} "$1" /time:30
   fi
+}
+
+# on gitbash (msys) we need to convert to windows style path for java
+function winpath {
+  echo "${1}" | sed -e 's/^\///' -e 's/\//\\/g' -e 's/^./\0:/'
 }
 
 cd "${DIR}" || { notify "Failed to cd to $DIR"; exit 1; }
@@ -40,13 +47,24 @@ if [[ -f $DIR/env.sh ]]; then
   source "$DIR/env.sh"
 fi
 
+if [[ "${OSTYPE}" == "msys" ]]; then
+  CLASSPATH="${JAR_NAME};$(winpath ${LIB_DIR})/*"
+  LD_PATH="$(winpath ${LIB_DIR})"
+
+  # Fixes bug  Unable to get Charset 'cp65001' for property 'sun.stdout.encoding'
+  JAVA_OPTS="${JAVA_OPTS} -Dsun.stdout.encoding=UTF-8 -Dsun.err.encoding=UTF-8"
+else
+  CLASSPATH="${JAR_NAME}:${LIB_DIR}/*"
+  LD_PATH="${LIB_DIR}"
+fi
+
 java -cp "${JAR_NAME}" $JAVA_OPTS se.alipsa.ride.splash.SplashScreen &
 
-# it is possible to force the initial packageloader by adding:
+# Note: It is possible to force the initial packageloader by adding:
 # -DConsoleComponent.PackageLoader=ClasspathPackageLoader
 # to the command below, but even better to add it to JAVA_OPTS variable in env.sh
 
-java -Djava.library.path="${LIB_DIR}" -cp "${JAR_NAME}:${LIB_DIR}/*" \
+java -Djava.library.path="${LD_PATH}" -cp "${CLASSPATH}" \
 -Dcom.github.fommil.netlib.BLAS=${BLAS} \
 -Dcom.github.fommil.netlib.LAPACK=${LAPACK} \
 -Dcom.github.fommil.netlib.ARPACK=${ARPACK} \
